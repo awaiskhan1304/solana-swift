bd488cbad6b0cb9e76a3d8f18ca3c7eef6cf105a
awaiskhan1304 <88824713+awaiskhan1304@users.noreply.github.com>
2024-09-12 14:55:26 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
+        .target(name: "secp256k1"),
         .target(
             name: "SolanaSwift",
             dependencies: [
